e16d003c2996d9b6833d412572f52d3d442d83dc
Buğra YÖNEV <98328954+bugrayonev@users.noreply.github.com>
2022-09-06 20:55:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function password_length(event){
+
+const bar = document.getElementById("password_length");
+
+var length = event.target.value.length;
+
+bar.className = "";
+
+if(length < 5 ){
+ bar.classList.add("bad");
+}
+else if (length >= 5 && length < 10){
+ bar.classList.add("good");
+}
+else if( length >= 10){
+ bar.classList.add("perfect");
+}
+}
